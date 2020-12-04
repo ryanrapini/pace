60497aa20ab328831f9f8469c55c4e26e8a5b650
Ryan Rapini <ryanrapini@gmail.com>
2020-12-04 13:55:45 -0600

    finished

--- resources/views/components/layout/app.blade.php
+++ resources/views/components/layout/app.blade.php
@@ -5,16 +5,14 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Pace</title>
+    <title>ClevelandPoet.com</title>
 
     <link rel="stylesheet" href="{{ $mix('/app.css') }}">
     <script src="{{ $mix('/app.js') }}" defer></script>
 </head>
 <body>
-    <x-layout.header />
-
-    <main class="flex flex-col items-center">
-        <div class="w-full max-w-xl">
+    <main class="flex h-screen">
+        <div class="m-auto">
             {{ $slot }}
         </div>
     </main>
--- resources/views/pages/posts/index.blade.php
+++
@@ -1,9 +0,0 @@
-<x-layout.app>
-    <h2 class="mt-6 font-semibold text-2xl">Posts</h2>
-
-    <ul class="mt-6">
-        <li>
-            <a href="/posts/example" class="text-xl text-blue-700 hover:underline">Example</a>
-        </li>
-    </ul>
-</x-layout.app>
